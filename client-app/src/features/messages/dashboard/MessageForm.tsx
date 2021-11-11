12f796ffadb130f6530396dc6c6348a585e89cdb
Yoni Erlich <cool80092@gmail.com>
2021-11-11 13:19:55 +0200

    yesss

--- client-app/src/features/messages/dashboard/MessageForm.tsx
+++ client-app/src/features/messages/dashboard/MessageForm.tsx
@@ -1,3 +1,4 @@
+import { Guid } from "guid-typescript";
 import React, { ChangeEvent, useState } from "react";
 import { Button, Form } from "semantic-ui-react";
 import { Message } from "../../../app/models/message";
@@ -11,7 +12,7 @@ interface Props {
 function MessageForm({message : createdMessage , createMessage} : Props)  {
 	
 	const initialState = createdMessage ?? {
-		messageId: '',
+		messageId: Guid.create().toString,
 		content: '',
 		dateSent: new Date().toString()
 
@@ -25,14 +26,18 @@ function MessageForm({message : createdMessage , createMessage} : Props)  {
 	}
 	
 	function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+		console.log(message);
+		// let newMessage = message;
+		// newMessage.content = event.target.value;
+		// setMessage(newMessage);
 		const {name, value} = event.target;
-		setMessage({...message, [name]: value});
+		setMessage({...message, content: value});
 	}
 	
         return (
             <Form onSubmit={handleSubmit} style={{ margin:"0 0 1em 0" }}>
 				<Form.Group inline style={{ flexWrap:"nowrap", margin:0 }}>
-					<Form.Input width={16}  value={message.content}  onChange={handleInputChange}  />
+					<Form.Input width={16}   onChange={handleInputChange}  />
 					<Button type='submit' primary>Send</Button>
 				</Form.Group>
 			</Form>
